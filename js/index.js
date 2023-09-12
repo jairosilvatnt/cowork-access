9e8efc2d1e4f04afd7f46d8d4940bd8957e6f14e
Jairo da Silva <silva_jairo2009@yahoo.com.br>
2023-09-12 14:30:10 -0300

    verificação de usuarios e senhas

--- js/index.js
+++ js/index.js
@@ -3,6 +3,21 @@
 
  let people = JSON.parse(localStorage.getItem('people'))||[];
 
+  // Função para verificar a senha
+function isPasswordValid(password) {
+  // Verifica se a senha tem pelo menos 8 caracteres
+  if (password.length < 8) {
+    return false;
+  }
+
+  // Verifica se a senha contém pelo menos uma letra
+  if (!/[a-zA-Z]/.test(password)) {
+    return false;
+  }
+
+  return true;
+}
+
  // Adiciona um ouvinte de evento para o envio do formulário
  form.addEventListener('submit', function(event) {
   event.preventDefault(); // Impede o envio do formulário
@@ -11,6 +26,22 @@
     const name = document.getElementById('name').value;
     const password = document.getElementById('password').value;
 
+     // Verifica se a senha é válida
+    if (!isPasswordValid(password)) {
+      console.log('A senha deve ter pelo menos 8 caracteres e conter pelo menos uma letra.');
+      form.reset();
+      return; // Não envia o formulário se a senha não for válida
+    }
+
+      // Verifica se o nome já existe na lista
+    const nameExists = people.some(person => person.name === name);
+
+    if (nameExists) {
+      console.log('Este nome já existe na lista!');
+      form.reset();
+      return; // Não adiciona a pessoa à lista
+    }
+    
     // Cria um objeto com os dados do formulário
     const person = {
       name,
