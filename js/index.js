8c41b034a0752e2a8273491e1a41610c2a03c571
Jairo da Silva <silva_jairo2009@yahoo.com.br>
2023-09-13 10:56:45 -0300

    acresentando novas funcionalidades

--- js/index.js
+++ js/index.js
@@ -1,38 +1,53 @@
-// Obtém o formulário
 const form = document.getElementById('form');
 const nameInput = document.getElementById('name');
+const cpfInput = document.getElementById('cpf');
+const phoneInput = document.getElementById('phone');
+const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 
 let people = JSON.parse(localStorage.getItem('people')) || [];
 
-// Função para verificar a senha
 function isPasswordValid(password) {
-  // Verifica se a senha tem pelo menos 8 caracteres e contém pelo menos uma letra
   return /^(?=.*[A-Za-z]).{8,}$/.test(password);
 }
 
-// Função para adicionar uma pessoa à lista
-function addPerson(name, password) {
-  const person = { name, password };
+function isValidCPF(cpf) {
+  // Verifica se o CPF tem 11 dígitos
+  if (cpf.length !== 11) {
+    return false;
+  }
+  return true;
+}
+
+function isValidEmail(email) {  
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
+
+function addPerson(name, cpf, phone, email, password) {
+  const person = { name, cpf, phone, email, password };
   people.push(person);
 }
 
-// Função para verificar se o nome já existe na lista
 function doesNameExist(name) {
   return people.some(person => person.name === name);
 }
 
-// Função para exibir uma mensagem no console e redefinir o formulário
-function displayMessageAndReset(message) {
-  alert(message);
-  form.reset();
+function doesCPFExist(cpf) {
+  return people.some(person => person.cpf === cpf);
 }
 
-// Adiciona um ouvinte de evento para o envio do formulário
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Impede o envio do formulário
+function alert(message) {
+  console.log(message);
+}
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
 
   const name = nameInput.value;
+  const cpf = cpfInput.value;
+  const phone = phoneInput.value;
+  const email = emailInput.value;
   const password = passwordInput.value;
 
   if (!isPasswordValid(password)) {
@@ -45,9 +60,26 @@ form.addEventListener('submit', function(event) {
     return;
   }
 
-  addPerson(name, password);
+  if (!isValidCPF(cpf)) {
+    alert('CPF inválido ou em formato incorreto.');
+    return;
+  }
 
+  if (doesCPFExist(cpf)) {
+    alert('Este CPF já está cadastrado!');
+    return;
+  }
+  
+  if (!isValidEmail(email)) {
+    alert('Por favor, insira um endereço de email válido.');
+    return;
+  }
+  
+  addPerson(name, cpf, phone, email, password);
+  
   localStorage.setItem('people', JSON.stringify(people));
-
+  
   alert('Dados do formulário armazenados no localStorage:');
+  form.reset();
 });
+
