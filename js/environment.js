629dc07845d60c420f707d7b7875a7cf508f7527
Jairo da Silva <silva_jairo2009@yahoo.com.br>
2023-09-16 12:55:16 -0300

    alterações

--- js/environment.js
+++ js/environment.js
@@ -1,17 +1,26 @@
+const peopleData = localStorage.getItem('people');
 const loginData = localStorage.getItem('login');
 const pUserName = document.getElementById('userName');
 const pUserEmail = document.getElementById('userEmail');
 const pUserPhone = document.getElementById('userPhone');
 
-// 
+if (peopleData !== null) {
+  const peopleObj = JSON.parse(peopleData);
+  peopleObj.filter(email);
+
+  const userName = peopleObj.name;
+  const userPhone = peopleObj.phone;
+
+  pUserName.textContent = userName;
+  pUserPhone.textContent = userPhone;
+};
 
 if (loginData !== null) {
   const loginObj = JSON.parse(loginData);
+
   const userEmail = loginObj.email;
-  const userName = loginObj.name;
- 
-  pUserEmail.textContent = userEmail;  
-  pUserName.textContent = userName;  
+  pUserEmail.textContent = userEmail;
+
 } else {
   window.location.href = 'login.html';
 }
